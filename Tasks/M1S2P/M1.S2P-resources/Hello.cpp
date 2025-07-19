d0b2bafb258ece8ccf0fcd1f4e59605d58c1947d
Jacob <jsb-dev@outlook.com>
2025-07-19 12:15:50 +1000

    Structure workspace

--- Tasks/M1S2P/M1.S2P-resources/Hello.cpp
+++ Tasks/M1S2P/M1.S2P-resources/Hello.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int size = 4;
+//
+int arraySize = 4;
 
 int sum(int n)
 {
     static int result = 0;
 
     result += n;
-    
+
     return result;
 }
 
@@ -17,20 +17,20 @@ int main()
     int *p;
     int i = 0;
 
-    p = new int[size];
+    p = new int[arraySize];
 
     int total = 0;
 
-    for (i = 0; i < size; i++)
+    for (i = 0; i < arraySize; i++)
     {
-        cout << "enter number " << i + 1  << ": ";
-        cin >> p[i];
+        std::cout << "enter number " << i + 1 << ": ";
+        std::cin >> p[i];
         total = sum(p[i]);
     }
 
     delete p;
 
-    cout<<"the final result is "<<total;
+    std::cout << "the final result is " << total;
 
     return 0;
 }
